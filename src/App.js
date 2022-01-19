a2c12094caee8447edcce4b3b0a475e91b7711fc
devmaxwel <eng.maxwel.ochieng@gmail.com>
2022-01-19 13:38:32 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [live, setLive] = useState();
+
+
+
+  const options = {
+  method: 'GET',
+  url: 'https://livescore6.p.rapidapi.com/matches/v2/list-live',
+  params: {Category: 'soccer'},
+  headers: {
+    'x-rapidapi-host': 'livescore6.p.rapidapi.com',
+    'x-rapidapi-key': 'b2e6cc5070mshc415f81e5aea8bcp133181jsnaa4516e7a28d'
+  }
+};
+
+const fetchLiveGames= async ()=> {
+    return await axios.request(options).then(function (response){
+       setLive(response.data)
+    })
+  
+}
+
+useEffect(() => {
+    fetchLiveGames();
+})
+
+console.log(live)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
